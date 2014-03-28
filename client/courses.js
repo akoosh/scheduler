bd1a30ffd0ef62a25938df80ab7e61bbb9a7b5cd
Zachary Thompson <zgthompson@gmail.com>
2014-03-28 15:53:59 -0700

    added search by professor to course model

--- client/courses.js
+++ client/courses.js
@@ -1,7 +1,11 @@
 Scheduler.Courses = {
 
 	is_course_title: function( title ) {
-		return CoursesModel.find( { "title": title } ).fetch().length > 0;
+		return CoursesModel.find( { "title": /^title/i }, { "_id": 1 } ).fetch().length > 0;
+	},
+
+	is_professor: function( professor ) {
+		return db.Courses.find( { "classes.sections.professors": /^professor/i }, { "_id": 1 } ).fetch().length > 0;
 	}
 };
 
