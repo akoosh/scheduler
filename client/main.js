240d3315496464939c262019ced6d9adc14237ad
Arthur Wuterich <ArthurWut@gmail.com>
2014-10-22 16:51:34 -0700

    Removed routing session variable and added iron:router to project. Routing information is located in routes.js in root directory.

--- client/main.js
+++ client/main.js
@@ -41,21 +41,3 @@ Template.query_page.events( {
         }
 }
 );
-
-Template.page_loader.helpers( {
-  "loadPage" : function(name) {
-    var page = Session.get("current_page");
-    if( typeof page === "undefined" ) {
-      page = "query_page";
-      Session.set( "current_page", page );
-    }
-
-    return { template: Template[page] };
-  },
-});
-
-Template.page_loader.events( {
-  "mouseup #schedule_transition" : function() {
-    Session.set( "currentPage", "schedule_page" );
-  },
-});
---
+++ routes.js
@@ -0,0 +1,9 @@
+// SSU_Scheduler routing
+// ZT & AW
+// 10/21/2014
+
+Router.map(function () {
+  this.route('query_page', {
+    path: '/' // match the root path
+  });
+});
