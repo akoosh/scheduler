6e61b215775e3c8850d61ba5777c27ef66ff0dbf
Arthur Wuterich <ArthurWut@gmail.com>
2014-10-22 17:07:39 -0700

    Modifying routing to work with scheduling page

--- routes.js
+++ routes.js
@@ -3,11 +3,15 @@
 // 10/21/2014
 
 Router.map(function () {
-  this.route('query_page', {
-    path: '/' // match the root path
+  
+
+  // Schedules
+  this.route('schedule_page', {
+    path: '/schedules' 
   });
 
-  this.route('schedule_page', {
-    path: '/schedules' // match the root path
+  // Home page
+  this.route('query_page', {
+    path: "/*",             // Match anything not matched
   });
 });
