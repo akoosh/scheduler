4fa5ead6bbf98eac0f679e2324ce190dd2cdf09f
Arthur Wuterich <ArthurWut@gmail.com>
2014-10-27 14:52:34 -0700

    Added conversion function to convert time values into [0-1] range

--- client/Schedules.js
+++ client/Schedules.js
@@ -25,6 +25,16 @@ Scheduler.Schedules = {
     return result;
   },
 
+  // Converts the passed time string to military time if applicable
+  "timeStringConvert" : function(timeString) {
+    var result = Scheduler.Schedules.timeStringToInt( timeString );
+    if( timeString.search(/pm/gi) != -1 && result < 0.5 ){
+      result += 0.5;
+    }
+
+    return result;
+  },
+
   "timeStringToInt" : function(timeString) {
     var val = 0;
 
@@ -38,45 +48,64 @@ Scheduler.Schedules = {
     }
 
     // Consume time string to build value
-    if( while timeString.length != 0 ) {
-      var c = timeString[0];
+    while( timeString.length != 0 ) {
+      var c = parseInt( timeString[0] );
       timeString = timeString.slice(1);
 
-      // PM case
       switch( timeString.length ) {
+        // X#:##
         case 3:
-          val += 0.5;
+          val += c * 0.41666666666666663;
         break;
 
+        // #X:##
         case 2:
+          val += c * 0.041666666666666664;
         break;
 
+        // ##:x#
         case 1:
+          val += c * 0.006944444444444444;
         break;
 
+        // ##:#x
         case 0:
+          val += c * 0.0006944444444444444;
         break;
       }
     }
 
-    val = timeString;
-    return val;
+    // Clamp val in the range [0, 1]
+    if( val < 0 ) {
+      val = 0;
+    } else if( val > 1 ) {
+      val = 1;
+    }
+
+    return Math.round( val * 10000 ) / 10000;
   },
 
   // Returns a array of time block objects converted into a general form
   // {time} => [ { day, start, end }, { day, start, end }, ... ]
   // where start and end are in the range [0-1] which represents 00:00 - 23:59
-  "generateTimeBlocks" : function( times ) {
+  "generateTimeBlocks" : function( courses ) {
     var result = [];
-    for( time in times ) {
-      time = times[time];
-      var timeString = time.days.split("");
+    for( course in courses ) {
+      course = courses[course];
+      var timeString = course.time.days.split("");
       for( day in timeString ) {
         day = timeString[day];
-        result.append( {
+        if( day == "H" ) {
+          result[result.length-1].day += day;
+          continue;
+        }
+
+        result.push( {
           "day"  : day,
-          "start": 0,
-          "end"  : 1
+          "start": Scheduler.Schedules.timeStringConvert( course.time.start_time ),
+          "end": Scheduler.Schedules.timeStringConvert( course.time.end_time ),
+          "start_raw": course.time.start_time,
+          "end_raw": course.time.end_time,
         });
       }
     }
