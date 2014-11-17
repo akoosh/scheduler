a82832928696db0331197a16653b552bc3c56217
Zachary Thompson <zgthompson@gmail.com>
2014-11-17 11:27:03 -0800

    add and remove classes to slot 1

--- client/main.js
+++ client/main.js
@@ -7,6 +7,29 @@ Template.queryDisplay.helpers(
     }
 );
 
+Template.courseDisplay.helpers(
+    {
+        augmentedClasses: function() {
+            var outerThis = this;
+            return _.map( outerThis.classes, 
+                function(cl) { 
+                    return _.extend(cl, { subject_with_number: outerThis.subject_with_number } ); 
+                }
+            );
+        }
+    }
+);
+
+Template.planLayout.helpers( 
+    {
+
+        "slots": function() {
+            var plan = Session.get("slots");
+            return plan || [];
+        }
+    }
+);
+
 Template.queryPage.events (
     {
         "keyup #query": function() {
@@ -27,6 +50,32 @@ Template.queryPage.events (
             }, 500 );
  
             Session.set("timeoutHander", new_hander);
+        },
+
+        "click .addButton": function() {
+            var slotSelected = Session.get("slotSelected") || 0;
+            var slots = Session.get("slots") || [];
+
+            var curSlot = slots[slotSelected] || [];
+
+            if (this.classes !== undefined) curSlot = curSlot.concat(this.classes);
+            else curSlot.push(this);
+
+            slots[slotSelected] = curSlot;
+
+            Session.set("slots", slots);
+        },
+
+        "click .removeButton": function() {
+            var slotSelected = Session.get("slotSelected") || 0;
+            var slots = Session.get("slots") || [];
+            var curSlot = slots[slotSelected] || [];
+
+            var outerThis = this;
+            curSlot = _.reject(curSlot, function(ele) { return ele.number == outerThis.number; });
+            slots[slotSelected] = curSlot;
+
+            Session.set("slots", slots);
         }
     }
 );
