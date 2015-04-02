b94fda40b8f7fe31bd7035a93b7699f3b3f5df7e
Arthur Wuterich <ArthurWut@gmail.com>
2015-04-02 11:12:51 -0700

    Added logic in updateDatabase.py to have a user specified config file; else, the default config file is loaded

--- scripts/updateDatabase.py
+++ scripts/updateDatabase.py
@@ -17,35 +17,40 @@ def meteorOn( url, port):
   return result
 
 def main():
+  cfg = None
   with open( "config.json", "r" ) as config:
     cfg = json.load( config )
-    if meteorOn( cfg["MeteorAddress"], cfg["MeteorPort"] ):
-      writeTime = str( int(time.time()) )
 
-      if "CourseData" in cfg and "SupCourseData" in cfg:
-        if os.path.isfile( cfg["CourseData"] ) and os.path.isfile( cfg["SupCourseData"] ):
-          print "Generating course data..."
-          cTJ( cfg["CourseData"], cfg["SupCourseData"] )
-          print "Done."
-
-          print "Importing class data into mongo..."
-          os.system( "mongoimport -h %s:%s --drop --db %s --collection Classes --type json --jsonArray --file classes.json && mv classes.json bkup/classes.%s.json" % ( cfg["MeteorAddress"], cfg["MeteorMongoPort"], cfg["MeteorDB"], writeTime ) )
-          print "Done."
-
-
-      if "StudentData" in cfg:
-        if os.path.isfile( cfg["StudentData"] ):
-          print "Generating student data..."
-          sTJ( cfg["StudentData"] )
-          print "Done."
-
-          print "Importing student data into mongo..."
-          os.system( "mongoimport -h %s:%s --drop --db %s --collection Students --type json --jsonArray --file students.json && mv students.json bkup/students.%s.json" % ( cfg["MeteorAddress"], cfg["MeteorMongoPort"], cfg["MeteorDB"], writeTime ) )
-          print "Done."
-
-    else:
-      print "The Meteor server needs to be running to allow access to the database"
+  if not cfg:
+    with open( ".config.json", "r" ) as config:
+      cfg = json.load( config )
     
+  if meteorOn( cfg["MeteorAddress"], cfg["MeteorPort"] ):
+    writeTime = str( int(time.time()) )
+
+    if "CourseData" in cfg and "SupCourseData" in cfg:
+      if os.path.isfile( cfg["CourseData"] ) and os.path.isfile( cfg["SupCourseData"] ):
+        print "Generating course data..."
+        cTJ( cfg["CourseData"], cfg["SupCourseData"] )
+        print "Done."
+
+        print "Importing class data into mongo..."
+        os.system( "mongoimport -h %s:%s --drop --db %s --collection Classes --type json --jsonArray --file classes.json && mv classes.json bkup/classes.%s.json" % ( cfg["MeteorAddress"], cfg["MeteorMongoPort"], cfg["MeteorDB"], writeTime ) )
+        print "Done."
+
+
+    if "StudentData" in cfg:
+      if os.path.isfile( cfg["StudentData"] ):
+        print "Generating student data..."
+        sTJ( cfg["StudentData"] )
+        print "Done."
+
+        print "Importing student data into mongo..."
+        os.system( "mongoimport -h %s:%s --drop --db %s --collection Students --type json --jsonArray --file students.json && mv students.json bkup/students.%s.json" % ( cfg["MeteorAddress"], cfg["MeteorMongoPort"], cfg["MeteorDB"], writeTime ) )
+        print "Done."
+
+  else:
+    print "The Meteor server needs to be running to allow access to the database"
 
 if __name__ == "__main__":
   main()
